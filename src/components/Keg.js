694217ca361a9d0cfd0831fef83c9b32edfbecb3
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-02-11 12:29:16 -0800

    Adjust formatting

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Keg(props) {
   return (
     <React.Fragment>
-      {props.name} - 
-      {props.brand} - 
-      {props.price} - 
-      {props.alcoholContent}
+      <div onClick = {() => props.whenKegClicked(props.id)}>
+      <hr/>
+        {props.name} - {props.brand} - ${props.price} - {props.alcoholContent}%
+      </div>
     </React.Fragment>
   )
 }
+
+Keg.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.string,
+  alcoholContent: PropTypes.string,
+  id: PropTypes.string,
+  whenKegClicked: PropTypes.func
+};
 
 export default Keg;
