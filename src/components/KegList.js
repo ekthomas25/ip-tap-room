bd5424121bc9b34ebf84711bdd83a7d50d5bb45f
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-02-11 12:30:41 -0800

    Add proptypes and function

---
+++ src/components/KegList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Keg from './Keg';
+import PropTypes from 'prop-types';
+
+function KegList(props) {
+  return (
+    <React.Fragment>
+      
+        {props.kegList.map((keg) =>
+          <Keg
+            whenKegClicked = {props.onKegSelection}
+            name={keg.name}
+            brand={keg.brand}
+            price={keg.price}
+            alcoholContent={keg.alcoholContent}
+            pintsRemaining={keg.pintsRemaining}
+            id={keg.id}
+            key={keg.id}
+          />  
+          )}
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+KegList.propTypes = {
+  kegList: PropTypes.array,
+  onKegSelection: PropTypes.func
+}
+
+export default KegList;
