78840707cad9436445da83541376ef028b88e2bd
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-02-13 09:17:57 -0800

    add headerStyle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
 function Header() {
+  const headerStyle = {
+    backgroundColor:'#3E3E3E',
+    color:'#FFFFFF',
+    top: '0',
+    width: '100%',
+    height: '6em',
+    marginTop: '-1.4em',
+    marginBottom: '3em',
+    paddingTop: '1.4em',
+    paddingLeft: '1.4em',
+  }
   return (
-    <h1>Nine Livers Tap Room</h1>
+    <React.Fragment>
+      <div style={headerStyle}>
+        <h1>Nine Livers Tap Room</h1>
+      </div>
+      <br/>
+    </React.Fragment>
   )
 }
 
